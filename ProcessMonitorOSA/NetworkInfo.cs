09d224077de90892d0d2193c0e7d128ff286106a
PeeWee2000 <RHarrisoN@themagnigroup.com>
2018-03-11 14:09:15 -0400

    Removed legacy version as SanD_Windows now encompasses it

---
+++ ProcessMonitorOSA/NetworkInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Diagnostics;
+using System.Drawing;
+
+
+namespace ProcessMonitorOSA
+{
+    class NetworkInfo
+    {
+        private static List<TcpProcessRecord> TcpActiveConnections = null;
+        private static List<UdpProcessRecord> UdpActiveConnections = null;
+        [DllImport("iphlpapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern uint GetExtendedTcpTable(IntPtr pTcpTable, ref int pdwSize,
+    bool bOrder, int ulAf, TcpTableClass tableClass, uint reserved = 0);
+
+        // The GetExtendedUdpTable function retrieves a table that contains a list of
+        // UDP endpoints available to the application. Decorating the function with
+        // DllImport attribute indicates that the attributed method is exposed by an
+        // unmanaged dynamic-link library 'iphlpapi.dll' as a static entry point.
+        [DllImport("iphlpapi.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern uint GetExtendedUdpTable(IntPtr pUdpTable, ref int pdwSize,
+            bool bOrder, int ulAf, UdpTableClass tableClass, uint reserved = 0);
+
+
+    }
+}
